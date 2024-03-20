64279646cd80a080d970f1b5ade8b6c3aed4f755
tayouellette <156235392+tayouellette@users.noreply.github.com>
2024-03-20 16:15:48 -0400

    Create 1946_knpsections_poach.js

---
+++ final project/1946_knpsections_poach.js
@@ -0,0 +1,111 @@
+var sections = {
+    "type": "FeatureCollection",
+    "name": "knp-shp-sections",
+    "features": [
+
+        {
+            "type": "Feature",
+            "properties": {
+              "Name": "Shangoni",
+              "Size_ha": null,
+              "Shape__Are": null,
+              "Shape__Len": null,
+              "name_sections_Lat": -23.24,
+              "name_sections_Long": 30.975,
+              "name_sections_Poaching": 2.1,
+              "PoachingData": [
+                {
+                  "Year": 1946,
+                  "Incidents": 10
+                }
+              ],
+              "RangerQuote": "At one time when small mines were in operation close to the western boundary of the Park this was one of the most difficult sections to control, there occurred constant poaching over its Western boundary on the part of both whites and natives from the mines, and by natives from the locations. Now that all the mines have closed down the first danger has been eliminated but the natives on the borders are ardent game killers and destroy many of our animals chiefly by snares when they stray far over the boundary. Indeed poachers have been caught 10 miles inside the boundary and it is only close guarding and patrolling that keeps these, people in check. Formerly, before we were in a position to safeguard this section by stationing a ranger there, large gangs with many dogs and even guns used to hunt far into the Park. At present the ranger of the No. 7 adjoining section is in charge and spends a portion of his time at Shangoni. This section is full of rare game such as buffalo and eland. There is no tourist rest camp and the ranger can therefore spend his undivided time looking after the safety of the wild animals.",
+              "RangerName": "Ranger Kirkman"
+            },
+            "geometry": {
+              "type": "MultiPolygon",
+              "coordinates": []
+            }
+          },
+          
+{ "type": "Feature", "properties": { "Name": "Shingwedzi", "Size_ha": null, "Shape__Are": null, "Shape__Len": null, "name_sections_Lat": -23.116667, "name_sections_Long": 31.4333, 
+"name_sections_Poaching": 1.23,
+"PoachingData": [
+    { "Year": 1946, "Incidents": 1 }
+    ],
+    "RangerQuote": "This, the largest section in the Park in point of mileage, is in the fortunate position of having few outside problems at present. The Portuguese side of the border is only thinly inhabited, and holds enough game to keep the natives at home. On the other sides it is bordered by the K,N. Park. Formerly gangs of native hunters equipped with dogs, snares and bows and arrows used to penetrate from the west, and camp far up the Shingwedsi River; but the present ranger, shortly after taking over effectively put a stop to this practice.",
+    "RangerName": "Ranger Tomlinson"
+     }, "geometry": { "type": "MultiPolygon", "coordinates": [ ] } },
+
+{ "type": "Feature", "properties": { "Name": "Letaba", "Size_ha": null, "Shape__Are": null, "Shape__Len": null, "name_sections_Lat": -23.851, "name_sections_Long": 31.57666, 
+"name_sections_Poaching": 1.2,
+"PoachingData": [
+    { "Year": 1946, "Incidents": 10 }
+    ],
+    "RangerQuote": "The chief danger point in this section is the western border, where there are some ranchers and a good many natives. At one time white poachers occupied land adjoining but have now transferred elsewhere. The foot and mouth guards who form a cordon not far outside the boundary on the West are said to shoot any game they can. The Letaba farmers’ association who are constantly voicing -complaints re game coming from the Park are too far away to cause any personal trouble to the staff and in fact game breeds and exists in this area more extensively outside than inside the Park. There is little trouble from natives along the very thinly inhabited Portuguese border.",
+    "RangerName": "Ranger Crous"
+}, "geometry": { "type": "MultiPolygon", "coordinates": [ ] } },
+
+
+{ "type": "Feature", "properties": { "Name": "Satara", "Size_ha": null, "Shape__Are": null, "Shape__Len": null, "name_sections_Lat": -24.3977, "name_sections_Long": 31.777, 
+"name_sections_Poaching": 4.13,
+"PoachingData": [
+    { "Year": 1946, "Incidents": 5 }
+],
+"RangerQuote": "This is also a very large section effective supervision of which throughout is handicapped by the long detached strip along the south bank of the Olifants River...The western boundary is lined by farms occupied in winter mainly by protectionists though there are a few believed to have been purchased by hunting syndicates. However there being a good supply of game outside the Park and very few native inhabitants little trouble has accrued from this side in recent years. On the east lies Portuguese Territory and formerly there used to be a great deal of poaching from that side just as there was in the case of No. 4 section (Ranger Steyn). In 1925 one of our native rangers was killed and another seriously injured by Portugues native poachers. Since that time, when the offenders were captured on our representations, and sentenced to 14 years imprisonment by the Portuguese authorities (only one out of 12 survived to be released!) there have been no serious cases reported to this office. The position is in fact much like that in No. 4 section, according to the rangers' reports from each Section. There is a good deal of native traffic, illegal and otherwise, from P.E.A. especially since the WNLA recruiting stations closed down, and their transport ceased functioning.",
+"RangerName": "Ranger Ledeboer"
+
+}, "geometry": { "type": "MultiPolygon", "coordinates": [ ] } },
+{ "type": "Feature", "properties": { "Name": "Tshokwane", "Size_ha": null, "Shape__Are": null, "Shape__Len": null, "name_sections_Lat": -24.76306, "name_sections_Long": 31.78222, 
+"name_sections_Poaching": 6.33,
+"PoachingData": [
+    { "Year": 1946, "Incidents": 5 }
+],
+"RangerQuote": "This is a large Section, and has on its west a number of farms occupied by white owners in the winter) and 9 on its east, Portugueso East Africa. Some of the white owners are game protectionists others are not. There are relatively few natives on the western border living on the private farms, and as there is plenty of game outside the Park, practically no trouble i experienced on that side, either from whites or natives. In former years - up to about 1929 - there used to be a good deal of poaching along the border on the east by Portuguese natives culminating in a shooting affray in that year when a Portuguese poacher was killed. Since that, so far as reports have reached the Warden's office, at least, matters have been very quiet, though some years ago a coloured storekeeper from P.E.A. was said to be shooting game across our border fairly constantly. Beyond that the ranger has not communicated any serious poaching which he would no doubt, have done had it come to his notice.",
+"RangerName": "Ranger Steyn"
+}, "geometry": { "type": "MultiPolygon", "coordinates": [ ] } },
+{ "type": "Feature", "properties": { "Name": "Skukuza", "Size_ha": null, "Shape__Are": null, "Shape__Len": null, "name_sections_Lat": -24.995277, "name_sections_Long": 31.5927777, 
+"name_sections_Poaching": 6.03,
+"PoachingData": [
+    { "Year": 1946, "Incidents": 0 }
+    ],
+"RangerQuote": "There are no special problems in this small section except keeping a watch on the native gangs working along the railway who do a good deal of snaring when they can. It is unnecessary to have a white ranger as such patrolling by native rangers as is required can be done from the Warden’s office."
+}, "geometry": { "type": "MultiPolygon", "coordinates": [ ] } },
+
+    { "type": "Feature", "properties": { "Name": "Pretoriuskop", "Size_ha": null, "Shape__Are": null, "Shape__Len": null, "name_sections_Lat": -25.172, "name_sections_Long": 31.269, 
+"name_sections_Poaching": 9.53,
+"PoachingData": [
+    { "Year": 1946, "Incidents": 15 }
+     ],
+    "RangerQuote": "Troubled by an immediately adjoining native area along the whole of its western and northern borders, the inhabitants of which are many of them extreme active poachers, not only adept at snaring but possessed of multitude of hunting dogs, and a not inconsiderable number of illicit firearms...Pretorius KOP part of the section bordering most nearly on the native area is open to the public all year round, is something that cannot be viewed without alarm",
+    "RangerName": "Ranger Wolhuter"
+    }, "geometry": { "type": "MultiPolygon", "coordinates": [ ] } },
+
+{ "type": "Feature", "properties": { "Name": "Malelane", "Size_ha": null, "Shape__Are": null, "Shape__Len": null, "name_sections_Lat": -25.472, "name_sections_Long": 31.5088, 
+"name_sections_Poaching": 1.8,
+"PoachingData": [
+    { "Year": 1946, "Incidents": 30 }
+ ],
+"RangerQuote": "The main hostile factor however, is the large native population, railway and faro employees in the west, and in the east, from Hectorspruit to the Section boundary, the inhabitants of the Tenbosch native area. A large proportion of the males in the latter are habitual idlers, and may be called professional poachers as they do no work, and the more prominent  among them are hardly out of gaol where they have served a sentence for poaching, than they are back again snaring game in the Park.",
+"RangerName": "Ranger James"
+}, "geometry": { "type": "MultiPolygon", "coordinates": [ ] } },
+{ "type": "Feature", "properties": { "Name": "Crocodile Bridge", "Size_ha": null, "Shape__Are": null, "Shape__Len": null, "name_sections_Lat": -25.3583, "name_sections_Long": 31.892, 
+"name_sections_Poaching": 5.93,
+"PoachingData": [
+    { "Year": 1946, "Incidents": 20 }
+     ],
+    "RangerQuote": "This is also a difficult Section, as owing to its proximity to the township of Komatipoort, the railway and the Portuguese border, there is a great deal of illicit traffic with the latter to contend with, both with whites and natives. It is also, at its western end, faced by the above-mentioned Tenbosch native area with its population of hunting natives, and the Selati Line runs through the middle of it, implying a constant watch to be kept on the S.A.R. employees on the line, especially the native working gangs. The present ranger is keen and competently carries out his difficult work, which also includes raids by native hunting parties (and sometimes white men) from Portuguese Territory.",
+    "RangerName": "Ranger McDonald"
+    }, "geometry": { "type": "MultiPolygon", "coordinates": [ ] } },
+
+{ "type": "Feature", "properties": { "Name": "Punda Maria", "Size_ha": null, "Shape__Are": null, "Shape__Len": null, "name_sections_Lat": -22.69211, "name_sections_Long": 31.01678, 
+"name_sections_Poaching": 0.3,
+"PoachingData": [
+    { "Year": 1946, "Incidents": 10 }
+ ],
+"RangerQuote": "The natives along the western border of this section in the locations form a threat which required constant vigilance. There are also occasional troubles with white men in connection with transit to and from Rhodesia and Portuguese Africa in hunting parties or on illegal recruiting. The Portuguese border is thinly inhabited, that territory carries game, and except in the very north, where population is denser, there is no trouble with natives from that side.",
+"RangerName": "Ranger Lamont"
+}, "geometry": { "type": "MultiPolygon", "coordinates": [ ] } }
+
+]
+}
